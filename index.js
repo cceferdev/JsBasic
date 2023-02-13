e1705dd909b1bec1c7a701ad7361ffcf334b0f59
CCEFERDEV <cefer.dev@gmail.com>
2023-02-13 14:50:01 -0400

    cambiosrecientes

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
-function add(x1, x2){
-return x1+ x2;
+const math = require('./math.js');
 
-}
-
-function substract (x1, x2) {
-return x1 - x2;
-
-}
-
-function multiply (x1, x2) {
-
-return x1 * x2;
-
-} 
-
-function divide (x1, x2){
-
-        if (x2 == 0){
-            console.log('no se puede dividir por cero');
-        }else{
-            return x1 / x2;
-        }
-
-}
-console.log(divide (1, 0));
+console.log(math.add (1, 0));
+console.log(math.substract (1, 0));
+console.log(math.divide (1, 0));
+console.log(math.multiply (1, 0));
--- math.js
+++ math.js
@@ -1,1 +1,29 @@
-sad
+function add(x1, x2){
+    return x1+ x2;
+    
+    }
+    
+    function substract (x1, x2) {
+    return x1 - x2;
+    
+    }
+    
+    function multiply (x1, x2) {
+    
+    return x1 * x2;
+    
+    } 
+    
+    function divide (x1, x2){
+    
+            if (x2 == 0){
+                console.log('no se puede dividir por cero');
+            }else{
+                return x1 / x2;
+            }
+    
+    }
+exports.add = add;
+exports.substract = substract;
+exports.multiply = multiply;
+exports.divide = divide;
