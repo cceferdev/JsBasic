6ad5584b71dac13258ba44b3695c7b837c1e9043
CCEFERDEV <cefer.dev@gmail.com>
2023-02-13 16:44:26 -0400

    updateV2.1

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const math = require('./math.js');
 
 console.log(math.add (1, 0));
 console.log(math.substract (1, 0));
-console.log(math.divide (1, 0));
-console.log(math.multiply (1, 0));
+console.log(math.divide (1, 5));
+console.log(math.multiply (1, 10));
