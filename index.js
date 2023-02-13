8e355a82bf1ebb4bbac9a77be27b174bf36fe3f9
CCEFERDEV <cefer.dev@gmail.com>
2023-02-13 13:47:14 -0400

    changeprojects

---
+++ index.js
@@ -0,0 +1,21 @@
+function add(x1, x2){
+return x1+ x2;
+
+}
+
+function substract (x1, x2) {
+return x1 - x2;
+
+}
+
+function multiply (x1, x2) {
+
+return x1 * x2;
+
+} 
+
+function divide (x1, x2){
+
+    return x1 / x2;
+
+}
