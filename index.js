ea5b52586fe4e34102bcdb34efadd9684d1ec048
CCEFERDEV <cefer.dev@gmail.com>
2023-02-13 13:59:27 -0400

    ChangeProjects

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ return x1 * x2;
 
 function divide (x1, x2){
 
-    return x1 / x2;
+        if (x2 == 0){
+            console.log('no se puede dividir por cero');
+        }else{
+            return x1 / x2;
+        }
 
 }
+console.log(divide (1, 0));
