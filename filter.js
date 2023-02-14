873e6c99551e9f50516f72f56788ea8987421e8b
cceferdev <cefer.dev@gmail.com>
2023-02-13 23:11:21 -0400

    documentacion

--- filter.js
+++ filter.js
@@ -2,8 +2,13 @@ const obtenerPokemones = async() => {
     try {
     const res = await fetch('https://pokeapi.co/api/v2/pokemon/')
     const data = await res.json()
-    const arrayNombres = data.results.filter(poke => poke.name === 'venusaur')
-    console.log(arrayNombres)
+    // discriminar objeto  
+    // const arrayNombres = data.results.filter(poke => poke.name !== 'venusaur')
+    
+    // apuntar objeto
+    // const arrayNombres = data.results.filter(poke => poke.name === 'venusaur')
+    
+    //verificar console.log(arrayNombres)
 }
     catch (error) {
         console.log(error)
