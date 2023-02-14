82d897ab463b290ae46b3f521a12b35a4b4abfa8
cceferdev <cefer.dev@gmail.com>
2023-02-13 22:32:08 -0400

    fetch+foreach+recorrido

--- fetch.js
+++ fetch.js
@@ -1,7 +1,8 @@
 fetch('https:/pokeapi.co/api/v2/pokemon/')
-    .then ( res => res.json()
-    )
+    .then ( res => res.json())
     .then(data =>{
-
-    }
-)
+        data.results.array.forEach(element => {
+            console.log(element.name)
+        });
+    })
+    .catch(error => console.log(error))
